8977572458be836d8334a04f87018eee9abf3f5f
Nathalia Cavalcante <nathalia.escossio@gmail.com>
2026-06-21 10:17:30 -0300

    Adicionando fixtures

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const { expect } = require('chai');
 require('dotenv').config();
 const { obterToken } = require('../helpers/autenticacao');
+const postTransferencias = require('../fixtures/postTransferencias.json');
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
@@ -12,30 +13,25 @@ describe('Transferencias', () => {
 
         it('Deve retornar 201 com o valor da transferencia quando o valor for igual ou maior que 10 reais', async () => {
 
-            const resposta = await request(process.env.BASE_URL)
-            .post('/transferencias')
-            .set('Content-Type', 'application/json')
-            .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 11.00,
-                token: ''
-            });
-            expect(resposta.status).to.equal(201);
-        });
-        it('Deve retornar erro 422 quando o valor da transferencia for menor que 10 reais', async () => {
+
+            const bodyTransferencias = { ...postTransferencias, valor: 11.00};
 
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: 1,
-                contaDestino: 2,
-                valor: 5.00,
-                token: ''
-            });
+            .send(bodyTransferencias);
+            expect(resposta.status).to.equal(201);
+        });
+        it('Deve retornar erro 422 quando o valor da transferencia for menor que 10 reais', async () => {
+
+
+            const bodyTransferencias = { ...postTransferencias, valor: 7.00};
+            const resposta = await request(process.env.BASE_URL)
+            .post('/transferencias')
+            .set('Content-Type', 'application/json')
+            .set('Authorization', `Bearer ${token}`)
+            .send(bodyTransferencias);
             expect(resposta.status).to.equal(422);
         });
     });
